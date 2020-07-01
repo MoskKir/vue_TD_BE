d165c3dd69ee06149b16dc3ab5ba0872dea01d5d
MoskKir <47816850+MoskKir@users.noreply.github.com>
2020-07-01 17:25:58 +0300

    fix: db table schema

--- src/server/db/db.sql
+++ src/server/db/db.sql
@@ -1,16 +1,17 @@
 CREATE TABLE todos(
     todo_id SERIAL PRIMARY KEY,
-    author VARCHAR(255),
-    title VARCHAR(255),
+    author_id INTEGER REFERENCES users(user_id) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    date_create timestamp
+    date_create timestamp,
+    status BOOLEAN,
 );
 
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
-    login VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255)
+    login VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users(login, email, password) VALUES ('root', 'root@root.com', 'toor');
