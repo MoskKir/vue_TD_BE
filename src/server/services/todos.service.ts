aef35ceafb2ba42de5662b2718342f011812049c
MoskKir <47816850+MoskKir@users.noreply.github.com>
2020-07-01 17:27:37 +0300

    fix: refactor todos service

--- src/server/services/todos.service.ts
+++ src/server/services/todos.service.ts
@@ -1,8 +1,10 @@
 import pool from '../db/db.connect';
 
+import ITodo from '../interfaces/todo.interface';
+
 export default class TodosService {
-    public static async getAllTodos() {
-        const allTodos = await pool.query('SELECT * FROM todos');
+    public static async getAllTodos(authorId :string) {
+        const allTodos = await pool.query(`SELECT * FROM todos WHERE author_id = ${authorId}`);
         return allTodos.rows;
     }
 
@@ -11,10 +13,10 @@ export default class TodosService {
         return todo.rows[0];
     }
 
-    public static async addNewTodo(body :any) {
+    public static async addNewTodo(todo :ITodo) {
         const newTodo = await pool.query(
-            'INSERT INTO todos (author, title, description, date_create) VALUES($1, $2, $3, NOW()) RETURNING *',
-            ['lorem_user', 'lorem_title', 'lorem_description']
+            'INSERT INTO todos (author_id, title, description, date_create, status) VALUES($1, $2, $3, NOW(), $4) RETURNING *',
+            [ todo.author_id, todo.title, todo.description, todo.status ]
         )
         return newTodo;
     }
